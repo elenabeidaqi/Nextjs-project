4a8197b18c8d2bb5118e280a5150ecdb26afd927
elenabeidaqi@gmail.com <elenabeidaqi@gmail.com>
2023-03-22 00:11:42 +0330

    implement all events page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import EventList from '@/components/events/event-list';
+import {getAllEvents} from "../../dummy-data";
 
 function events() {
+  const events = getAllEvents();
   return (
-    <div>All events page</div>
+    <EventList items={events}/>
   )
 }
 
