97abdc044f6a89489ceb984736c780b67a768efc
elenabeidaqi@gmail.com <elenabeidaqi@gmail.com>
2023-03-22 17:38:52 +0330

    Implement filter events

--- pages/events/[...eventsSlugs].tsx
+++ pages/events/[...eventsSlugs].tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function eventsSlugs() {
   return (
     <div>
-        events Slugs
+        Filtered Events
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import {getFeaturedEvents} from "../dummy-data";
 import EventList from "@/components/events/event-list";
-
+import EventSearch from "@/components/events/event.search";
+import { useRouter } from "next/router";
 export default function Home() {
-  const featuredEvents = getFeaturedEvents()
+  const featuredEvents = getFeaturedEvents();
+  const router = useRouter();
+  const findEventsHandler =(year : any , month : any)=>{
+    const findpath = `/events/${year}/${month}`
+    router.push(findpath);
+  }
   return (
-    <div>
+    <>
+      <EventSearch onSearch ={findEventsHandler}/>
       <EventList items={featuredEvents}/>
-    </div>
+    </>
   )
 }
